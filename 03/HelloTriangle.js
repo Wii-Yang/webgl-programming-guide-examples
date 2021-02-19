4d888d0920b332a485e58c828d8d7430691e505b
Wii-Yang <826085873@qq.com>
2021-02-19 16:25:36 +0800

    新增-绘制三角形

---
+++ 03/HelloTriangle.js
@@ -0,0 +1,78 @@
+// 顶点着色器
+const VSHADER_SOURCE =
+    'attribute vec4 a_Position;\n' +
+    'void main() {\n' +
+    '  gl_Position = a_Position;\n' +
+    '}\n'
+
+// 片元着色器
+const FSHADER_SOURCE =
+    'void main() {\n' +
+    ' gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' + // 设置颜色
+    '}\n';
+
+function main() {
+    // 获取<canvas>元素
+    const canvas = document.getElementById('webgl');
+
+    // 获取WebGL绘图上下文
+    const gl = getWebGLContext(canvas);
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+
+    // 初始化着色器
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to initialize shaders.')
+        return;
+    }
+
+    // 设置顶点位置
+    const n = initVertexBuffers(gl);
+    if (n < 0) {
+        console.log('Failed to set the positions of the vertices');
+        return;
+    }
+
+    // 设置<canvas>的背景色
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    // 清空<canvas>
+    gl.clear(gl.COLOR_BUFFER_BIT)
+
+    // 绘制三角形
+    gl.drawArrays(gl.TRIANGLES, 0, n)
+}
+
+function initVertexBuffers(gl) {
+    const vertices = new Float32Array([0.0, 0.5, -0.5, -0.5, 0.5, -0.5])
+    const n = 3 // 点的个数
+
+    // 创建缓冲区对象
+    const vertexBuffer = gl.createBuffer();
+    if (!vertexBuffer) {
+        console.log('Failed to create the buffer object');
+        return -1;
+    }
+
+    // 将缓冲区对象绑定到目标
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    // 向缓冲区对象中写入数据
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+    const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+
+    if (a_Position < 0) {
+        console.log('Failed to get the storage location of a_Position');
+        return -1;
+    }
+
+    // 将缓冲区对象分配给a_Position变量
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+
+    // 连接a_Position变量
+    gl.enableVertexAttribArray(a_Position);
+
+    return n;
+}
