4a9a5ff84378620388aac4a38210d2ed8905aeaa
Wii-Yang <826085873@qq.com>
2021-02-20 17:22:33 +0800

    新增-彩色三角形

---
+++ 05/ColoredTriangle.js
@@ -0,0 +1,93 @@
+// ColoredTriangle.js
+// 顶点着色器程序
+const VSHADER_SOURCE =
+    'attribute vec4 a_Position;\n' +
+    'attribute vec4 a_Color;\n' +
+    'varying vec4 v_Color;\n' + // varying变量
+    'void main() {\n' +
+    '  gl_Position = a_Position;\n' +
+    '  v_Color = a_Color;\n' + // 将数据传给片元着色器
+    '}\n';
+
+// 片元着色器
+const FSHADER_SOURCE =
+    'precision mediump float;\n' +
+    'varying vec4 v_Color;\n' +
+    'void main() {\n' +
+    ' gl_FragColor = v_Color;\n' + // 从顶点着色器接收数据
+    '}\n';
+
+function main() {
+    // 获取<canvas>元素
+    const canvas = document.getElementById('webgl');
+
+    // 获取WebGL绘图上下文
+    const gl = getWebGLContext(canvas);
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+
+    // 初始化着色器
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to initialize shaders.')
+        return;
+    }
+
+    // 设置顶点信息
+    const n = initVertexBuffers(gl);
+    if (n < 0) {
+        console.log('Failed to set the positions of the vertices');
+        return;
+    }
+
+    // 设置<canvas>的背景色
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    // 清空<canvas>
+    gl.clear(gl.COLOR_BUFFER_BIT)
+
+    // 绘制三个点
+    gl.drawArrays(gl.TRIANGLES, 0, n)
+}
+
+function initVertexBuffers(gl) {
+    const verticesColors = new Float32Array([
+        // 顶点坐标和颜色
+        0.0, 0.5, 1.0, 0.0, 0.0,
+        -0.5, -0.5, 0.0, 1.0, 0.0,
+        0.5, -0.5, 0.0, 0.0, 1.0
+    ])
+    const n = 3; // 顶点数量
+
+    // 创建缓冲区对象
+    const vertexColorBuffer = gl.createBuffer();
+    if (!vertexColorBuffer) {
+        console.log('Failed to create the buffer object');
+        return -1;
+    }
+    // 将顶点坐标和颜色写入缓冲区对象
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, verticesColors, gl.STATIC_DRAW);
+
+    const FSIZE = verticesColors.BYTES_PER_ELEMENT;
+    // 获取a_Position的存储位置，分配缓冲区并开启
+    const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+        console.log('Failed to get the storage location of a_Position');
+        return -1;
+    }
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 5, 0);
+    gl.enableVertexAttribArray(a_Position);
+
+    // 获取a_Color的存储位置，分配缓冲区并开启
+    const a_Color = gl.getAttribLocation(gl.program, 'a_Color');
+    if (!a_Color) {
+        console.log('Failed to get the storage location of a_Color');
+        return -1;
+    }
+    gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE * 5, FSIZE * 2);
+    gl.enableVertexAttribArray(a_Color);
+
+    return n;
+}
